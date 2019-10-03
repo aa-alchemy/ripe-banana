820a860d0ca1275142471f944803cb3a86052c75
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-03 14:27:40 -0700

    working on passing my reviewers test

--- e2e/__tests__/reviewers.test.js
+++ e2e/__tests__/reviewers.test.js
@@ -11,12 +11,81 @@ describe('reviewer api', () => {
     company: 'Evil Vampire'
   };
 
+  const aa2Review = {
+    rating: 4,
+    reviewer: [],
+    review: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    film: []
+  };
+
+  const aa2Film = {
+    title: 'AA2 Alchemist',
+    studio: [],
+    released: 2019,
+    cast: [
+      {
+        role: 'Lead Alchemist',
+        actor: []
+      }
+    ]
+  };
+
+  const aa2Studio = {
+    name: 'AA2',
+    address: {
+      city: 'Portland',
+      state: 'Oregon',
+      country: 'USA'
+    }
+  };
+
+  const aa2Actor = {
+    name: 'Antonella',
+    pob: 'Colombia'
+  };
+
   function postReviewer(reviewer) {
     return request
       .post('/api/reviewers')
       .send(reviewer)
       .expect(200)
       .then(({ body }) => body);
+  }
+
+  function postReview(aa2Review) {
+    return request
+      .post('/api/reviewers')
+      .send(aa2Reviewer)
+      .expect(200)
+      .then(({ body }) => {
+        aa2Review.reviewer = body._id;
+        return request
+          .post('/api/studios')
+          .send(aa2Studio)
+          .expect(200)
+          .then(({ body }) => {
+            aa2Film.studio[0] = body._id;
+            return request
+              .post('/api/actors')
+              .send(aa2Actor)
+              .expect(200)
+              .then(({ body }) => {
+                aa2Film.cast[0].actor = body._id;
+                return request
+                  .post('/api/films')
+                  .send(aa2Film)
+                  .expect(200)
+                  .then(({ body }) => {
+                    aa2Review.film = body._id;
+                    return request
+                      .post('/api/reviews')
+                      .send(aa2Review)
+                      .expect(200)
+                      .then(({ body }) => body);
+                  });
+              });
+          });
+      });
   }
 
   it('posts a reviewer', () => {
@@ -43,6 +112,12 @@ describe('reviewer api', () => {
       })
       .then(({ body }) => {
         expect(body.length).toBe(3);
+        expect(body[0]).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          name: aa2Reviewer.name,
+          company: aa2Reviewer.company
+        });
       });
   });
 
