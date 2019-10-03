612a0f2ec2703b21bb3f11132e9018773f252414
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-03 15:32:12 -0700

    I am domne with testing

--- e2e/__tests__/actors.test.js
+++ e2e/__tests__/actors.test.js
@@ -11,6 +11,27 @@ describe('actor api', () => {
     pob: 'Colombia'
   };
 
+  const aa2Film = {
+    title: 'AA2 Alchemist',
+    studio: [],
+    released: 2019,
+    cast: [
+      {
+        role: 'Lead Alchemist',
+        actor: []
+      }
+    ]
+  };
+
+  const aa2Studio = {
+    name: 'AA2',
+    address: {
+      city: 'Portland',
+      state: 'Oregon',
+      country: 'USA'
+    }
+  };
+
   function postActor(actor) {
     return request
       .post('/api/actors')
@@ -19,15 +40,36 @@ describe('actor api', () => {
       .then(({ body }) => body);
   }
 
-  it('posts an actor', () => {
-    return postActor(aa2Actor)
-      .then(actor => {
-        expect(actor).toEqual({
-          _id: expect.any(Object),
-          __v: 0,
-          ...actor
-        });
+  function postFilm(aa2Film) {
+    return request
+      .post('/api/studios')
+      .send(aa2Studio)
+      .expect(200)
+      .then(({ body }) => {
+        aa2Film.studio[0] = body._id;
+        return request
+          .post('/api/actors')
+          .send(aa2Actor)
+          .expect(200)
+          .then(({ body }) => {
+            aa2Film.cast[0].actor = body._id;
+            return request
+              .post('/api/films')
+              .send(aa2Film)
+              .expect(200)
+              .then(({ body }) => body);
+          });
       });
+  }
+
+  it('posts an actor', () => {
+    return postActor(aa2Actor).then(actor => {
+      expect(actor).toEqual({
+        _id: expect.any(Object),
+        __v: 0,
+        ...actor
+      });
+    });
   });
 
   it('gets all actors', () => {
@@ -37,28 +79,47 @@ describe('actor api', () => {
       postActor(aa2Actor)
     ])
       .then(() => {
-        return request
-          .get('/api/actors')
-          .expect(200);
+        return request.get('/api/actors').expect(200);
       })
       .then(({ body }) => {
         expect(body.length).toBe(3);
         expect(body[0]).toEqual({
           _id: expect.any(String),
-          __v: 0,
-          ...aa2Actor
+          name: aa2Actor.name
         });
       });
   });
 
   it('gets actor by id', () => {
-    return postActor(aa2Actor)
-      .then(actor => {
+    return postFilm(aa2Film)
+      .then(film => {
         return request
-          .get(`/api/actors/${actor._id}`)
+          .get(`/api/actors/${film.cast[0].actor}`)
           .expect(200)
           .then(({ body }) => {
-            expect(body).toEqual(actor);
+            expect(body).toMatchInlineSnapshot(
+              {
+                _id: expect.any(String),
+                films: [
+                  {
+                    _id: expect.any(String)
+                  }
+                ]
+              },
+              `
+            Object {
+              "_id": Any<String>,
+              "films": Array [
+                Object {
+                  "_id": Any<String>,
+                  "title": "AA2 Alchemist",
+                },
+              ],
+              "name": "Antonella",
+              "pob": "Colombia",
+            }
+          `
+            );
           });
       });
   });
@@ -72,4 +133,17 @@ describe('actor api', () => {
       });
   });
 
+  it('will throw error if trying to delete actor that is part of film', () => {
+    return postFilm(aa2Film)
+      .then(film => {
+        return request
+          .get(`/api/actors/${film.cast[0].actor}`)
+          .expect(200)
+          .then(actor => {
+            return request
+              .delete(`/api/actors/${actor._id}`)
+              .expect(400);
+          });
+      });
+  });
 });
